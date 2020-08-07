2defb5ddf4183d4a4008fd84a27d23275be0cd84
Darren Tebo <darrentebo83@outlook.com>
2020-08-07 14:10:59 -0400

    built axiosWithAuth utility file

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({component: Component, ...props}) => {
-    const token = localStorage.getItem('authToken');
-
     return(
-        token ? (
-            <Route path='/protected' {...props} />
-        ) : (
-            <Redirect to='/login' />
-        )
-    );
+        <Route
+            {...props}
+            render={() => {
+                localStorage.getItem('authToken') ? (
+                    <Component />
+                ) : (
+                    <Redirect to='login' />
+                )
+            }}
+        />
+    )
 };
 
 export default PrivateRoute;
